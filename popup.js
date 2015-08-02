e9e627d80bf9bfa5aea5970c08be2447f27ff2ab
zambrey <ameyazambre@Ameyas-MacBook-Air.local>
2015-08-02 11:51:18 -0700

    Removing google cast code
    Turns out tab cannot be cast directly from an extension. It has to be
    from a regular web page.
    http://stackoverflow.com/a/22160416/2154178

--- popup.js
+++ popup.js
@@ -4,8 +4,7 @@
  * ameyazambre@gmail.com
  */
 var backgroundPage = chrome.extension.getBackgroundPage(),
-	popupObject = new PopupObject(),
-	castManager = new CastManager();
+	popupObject = new PopupObject();
 
 {
 	setTimeout(popupObject.PopupRenderManager.initRender, 50);
@@ -486,13 +485,6 @@ function PopupInteractionManager()
 				popupObject.PopupRenderManager.switchViewStyle();
 			}
 		})
-		$("#castIcon").click(function()
-		{
-			if(castManager)
-			{
-				castManager.initializeSession();
-			}
-		})
 	}
 	return interactionObject;
 }
@@ -608,70 +600,4 @@ function SearchManager()
 		return mo;
 	}
 	return searchObject;
-}
-
-/*Chromecast support*/
-window['__onGCastApiAvailable'] = function(loaded, errorInfo) {
-  if (loaded) 
-  {
-    castManager.initializeCastAPI();
-  } 
-  else 
-  {
-    console.log(errorInfo);
-  }
-}
-
-function CastManager()
-{
-}
-
-CastManager.prototype.sessionListener = function(e)
-{
-	console.log("Session Listener");
-}
-
-CastManager.prototype.receiverListener = function(e)
-{
-	if(e === chrome.cast.ReceiverAvailability.AVAILABLE)
-	{
-		console.log("Receivers are available.");
-		//Show cast button
-	}
-	else
-	{
-		//Don't show cast button
-	}
-}
-
-CastManager.prototype.onAPIInitSuccess = function()
-{
-	console.log("Cast API initialized successfully.");
-}
-
-CastManager.prototype.onAPIInitError = function()
-{
-	console.log("Cast API initialization failed.");
-}
-
-CastManager.prototype.initializeCastAPI = function(e)
-{
-	var sessionRequest = new chrome.cast.SessionRequest(chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID),
-		apiConfig = new chrome.cast.ApiConfig(sessionRequest,this.sessionListener,this.receiverListener);
-	chrome.cast.initialize(apiConfig, this.onAPIInitSuccess, this.onAPIInitError);
-}
-
-CastManager.prototype.initializeSession = function()
-{
-	chrome.cast.requestSession(this.onRequestSessionSuccess, this.onLaunchError);
-}
-
-CastManager.prototype.onRequestSessionSuccess = function()
-{
-	console.log("Request session success");
-}
-
-CastManager.prototype.onLaunchError = function()
-{
-	console.log("Launch error");
 }
